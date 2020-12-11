eb646bf716ac9105ac82a16aee773756379eee2b
Yuuki Takano <ytakanoster@gmail.com>
2020-12-12 01:06:23 +0900

    implementing write transaction

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 mod tl2;
 
 fn main() {
+    let mut mem = tl2::Memory::new();
+    while !mem.write_transaction(|t| {
+        let mut f1 = t.load(0);
+        let mut f2 = t.load(8);
+        if f1[0] == 0 && f2[0] == 0 {
+            f1[0] = 1;
+            f2[0] = 1;
+            t.store(0, f1);
+            t.store(8, f2);
+            true
+        } else {
+            false
+        }
+    }) {}
+
     println!("Hello, world!");
 }
--- src/tl2.rs
+++ src/tl2.rs
@@ -1,46 +1,111 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::ptr::copy_nonoverlapping;
 use std::sync::atomic::AtomicU64;
 
-const STRIPE_SIZE: usize = 128;
-const MEM_SIZE: usize = 1024 * 1024;
+const STRIPE_SIZE: usize = 8; // u64, 8B
+const MEM_SIZE: usize = 128;
 
 pub struct Memory {
-    mem: *mut u8,
-    lock_ver: Vec<AtomicU64>,
+    mem: Vec<u8>,
+    lock_ver: Vec<AtomicU64>, // write-locks
+    global_clock: AtomicU64,
+    shift_size: usize,
 }
 
-thread_local!(
-    static READ_VERSION: RefCell<usize> = RefCell::new(0);
-    static READ_SET: RefCell<HashSet<usize>> = RefCell::new(HashSet::new());
-    static WRITE_SET: RefCell<HashMap<usize, [u8; STRIPE_SIZE]>> = RefCell::new(HashMap::new());
-);
-
 impl Memory {
-    // data load for write transaction
-    pub fn wtr_load(&mut self, addr: usize) -> [u8; STRIPE_SIZE] {
-        let mut mem = [0; STRIPE_SIZE];
-        if let Some(m) = WRITE_SET.with(move |s| {
-            if let Some(m) = s.borrow_mut().get(&addr) {
-                unsafe { copy_nonoverlapping(m.as_ptr(), mem.as_mut_ptr(), STRIPE_SIZE) };
+    pub fn new() -> Memory {
+        let mem = [0].repeat(MEM_SIZE);
+        let mut lock_ver = Vec::new();
+
+        let mut shift = 0;
+        loop {
+            if STRIPE_SIZE & (1 << shift) > 0 {
+                break;
             }
-            Some(mem)
-        }) {
-            return m;
+            shift += 1;
         }
 
-        let mut mem = [0; STRIPE_SIZE];
-        let src = (self.mem as usize + addr) as *const u8;
-        unsafe { copy_nonoverlapping(src, mem.as_mut_ptr(), STRIPE_SIZE) };
-        mem
+        for _ in 0..MEM_SIZE >> shift {
+            lock_ver.push(AtomicU64::new(0));
+        }
+
+        Memory {
+            mem: mem,
+            lock_ver: lock_ver,
+            global_clock: AtomicU64::new(0),
+            shift_size: shift,
+        }
     }
 
-    // data store for write transaction
-    pub fn wtr_store(&mut self, dst: usize, src: [u8; STRIPE_SIZE]) {
-        WRITE_SET.with(|s| {
-            s.borrow_mut().insert(dst, src);
-        });
+    pub fn write_transaction<F, R>(&mut self, f: F) -> R
+    where
+        F: FnOnce(&mut WriteTrans) -> R,
+    {
+        loop {
+            let mut trans = WriteTrans::new(self);
+            let result = f(&mut trans);
+            if !trans.is_abort {
+                return result;
+            }
+        }
+    }
+
+    fn test_not_modify(&mut self, addr: usize, rv: u64) -> bool {
+        let n = *self.lock_ver[addr >> self.shift_size].get_mut();
+        n & (1 << 63) == 0 && n <= rv
+    }
+}
+
+pub struct WriteTrans<'a> {
+    read_ver: u64,
+    read_set: HashSet<usize>,
+    write_set: HashMap<usize, [u8; STRIPE_SIZE]>,
+    is_abort: bool,
+    mem: &'a mut Memory,
+}
+
+impl<'a> WriteTrans<'a> {
+    fn new(mem: &mut Memory) -> WriteTrans {
+        WriteTrans {
+            read_ver: *mem.global_clock.get_mut(),
+            read_set: HashSet::new(),
+            write_set: HashMap::new(),
+            is_abort: false,
+            mem: mem,
+        }
+    }
+
+    pub fn load(&mut self, addr: usize) -> Option<[u8; STRIPE_SIZE]> {
+        if self.is_abort {
+            return None;
+        }
+
+        self.read_set.insert(addr);
+
+        // read from write-set
+        if let Some(m) = self.write_set.get(&addr) {
+            return Some(*m);
+        }
+
+        // read from memory
+        let mut mem = [0; STRIPE_SIZE];
+        for (dst, src) in mem
+            .iter_mut()
+            .zip(self.mem.mem[addr..addr + STRIPE_SIZE].iter())
+        {
+            *dst = *src;
+        }
+
+        // post validation
+        if !self.mem.test_not_modify(addr, self.read_ver) {
+            self.is_abort = true;
+            return None;
+        }
+
+        Some(mem)
+    }
+
+    pub fn store(&mut self, addr: usize, val: [u8; STRIPE_SIZE]) {
+        self.write_set.insert(addr, val);
     }
 }
