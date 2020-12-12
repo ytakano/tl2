f55cbe052b9fdb8d9bcb5c51a81242e539e3092f
Yuuki Takano <ytakanoster@gmail.com>
2020-12-12 18:15:26 +0900

    write transaction is available TODO: overflow of the version number read transaction efficient global clock

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,60 @@
+use std::sync::Arc;
+use std::{thread, time};
+
 mod tl2;
 
-fn main() {
-    let mut mem = tl2::Memory::new();
-    while !mem.write_transaction(|t| {
-        let mut f1 = t.load(0);
-        let mut f2 = t.load(8);
-        if f1[0] == 0 && f2[0] == 0 {
-            f1[0] = 1;
-            f2[0] = 1;
-            t.store(0, f1);
-            t.store(8, f2);
-            true
-        } else {
-            false
-        }
-    }) {}
+const NUM_PHILOSOPHERS: usize = 2;
 
-    println!("Hello, world!");
+fn philosopher(stm: Arc<tl2::STM>, n: usize) {
+    let left = 8 * n;
+    let right = 8 * ((n + 1) % NUM_PHILOSOPHERS);
+
+    for _ in 0..1000000 {
+        // pickup chopsticks
+        while !stm.write_transaction(|tr| {
+            let mut f1 = load!(tr, left);
+            let mut f2 = load!(tr, right);
+            if f1[0] == 0 && f2[0] == 0 {
+                f1[0] = 1;
+                f2[0] = 1;
+                store!(tr, left, f1);
+                store!(tr, right, f2);
+                tl2::TMResult::Ok(true)
+            } else {
+                tl2::TMResult::Ok(false)
+            }
+        }) {
+            println!("#{} failed to pickup", n);
+            let ten_millis = time::Duration::from_micros(1);
+            thread::sleep(ten_millis);
+        }
+
+        println!("#{} is eating", n);
+
+        // release chopsticks
+        stm.write_transaction(|tr| {
+            let mut f1 = load!(tr, left);
+            let mut f2 = load!(tr, right);
+            f1[0] = 0;
+            f2[0] = 0;
+            store!(tr, left, f1);
+            store!(tr, right, f2);
+            tl2::TMResult::Ok(())
+        });
+    }
+}
+
+fn main() {
+    let stm = Arc::new(tl2::STM::new());
+    let mut v = Vec::new();
+
+    for i in 0..NUM_PHILOSOPHERS {
+        let s = stm.clone();
+        let th = std::thread::spawn(move || philosopher(s, i));
+        v.push(th);
+    }
+
+    for th in v {
+        th.join().unwrap();
+    }
 }
--- src/tl2.rs
+++ src/tl2.rs
@@ -1,15 +1,39 @@
+use std::cell::UnsafeCell;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::sync::atomic::AtomicU64;
+use std::sync::atomic::{AtomicU64, Ordering};
 
 const STRIPE_SIZE: usize = 8; // u64, 8B
 const MEM_SIZE: usize = 128;
+
+#[macro_export]
+macro_rules! load {
+    ($t:ident, $a:expr) => {
+        if let Some(v) = ($t).load($a) {
+            v
+        } else {
+            return tl2::TMResult::Retry;
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! store {
+    ($t:ident, $a:expr, $v:expr) => {
+        $t.store($a, $v)
+    };
+}
 
 pub struct Memory {
     mem: Vec<u8>,
     lock_ver: Vec<AtomicU64>, // write-locks
     global_clock: AtomicU64,
     shift_size: usize,
+}
+
+pub enum TMResult<T> {
+    Ok(T),
+    Retry,
 }
 
 impl Memory {
@@ -37,22 +61,36 @@ impl Memory {
         }
     }
 
-    pub fn write_transaction<F, R>(&mut self, f: F) -> R
-    where
-        F: FnOnce(&mut WriteTrans) -> R,
-    {
-        loop {
-            let mut trans = WriteTrans::new(self);
-            let result = f(&mut trans);
-            if !trans.is_abort {
-                return result;
-            }
+    fn test_not_modify(&self, addr: usize, rv: u64) -> bool {
+        let n = self.lock_ver[addr >> self.shift_size].load(Ordering::Relaxed);
+        n & (1 << 63) == 0 && n <= rv
+    }
+
+    fn get_addr_ver(&self, addr: usize) -> u64 {
+        let n = self.lock_ver[addr >> self.shift_size].load(Ordering::Relaxed);
+        n & !(1 << 63)
+    }
+
+    fn lock_addr(&mut self, addr: usize) -> bool {
+        match self.lock_ver[addr >> self.shift_size].fetch_update(
+            Ordering::Relaxed,
+            Ordering::Relaxed,
+            |val| {
+                let n = val & (1 << 63);
+                if n == 0 {
+                    Some(val | (1 << 63))
+                } else {
+                    None
+                }
+            },
+        ) {
+            Ok(_) => true,
+            Err(_) => false,
         }
     }
 
-    fn test_not_modify(&mut self, addr: usize, rv: u64) -> bool {
-        let n = *self.lock_ver[addr >> self.shift_size].get_mut();
-        n & (1 << 63) == 0 && n <= rv
+    fn unlock_addr(&mut self, addr: usize) {
+        self.lock_ver[addr >> self.shift_size].fetch_and(!(1 << 63), Ordering::Relaxed);
     }
 }
 
@@ -60,6 +98,7 @@ pub struct WriteTrans<'a> {
     read_ver: u64,
     read_set: HashSet<usize>,
     write_set: HashMap<usize, [u8; STRIPE_SIZE]>,
+    locked: Vec<usize>,
     is_abort: bool,
     mem: &'a mut Memory,
 }
@@ -67,10 +106,14 @@ pub struct WriteTrans<'a> {
 impl<'a> WriteTrans<'a> {
     fn new(mem: &mut Memory) -> WriteTrans {
         WriteTrans {
-            read_ver: *mem.global_clock.get_mut(),
             read_set: HashSet::new(),
             write_set: HashMap::new(),
+            locked: Vec::new(),
             is_abort: false,
+
+            // 1. Sample global version-clock
+            read_ver: mem.global_clock.load(Ordering::Acquire),
+
             mem: mem,
         }
     }
@@ -107,5 +150,119 @@ impl<'a> WriteTrans<'a> {
 
     pub fn store(&mut self, addr: usize, val: [u8; STRIPE_SIZE]) {
         self.write_set.insert(addr, val);
+    }
+
+    fn transaction<F, R>(&mut self, f: F) -> TMResult<R>
+    where
+        F: Fn(&mut WriteTrans) -> TMResult<R>,
+    {
+        // 2. Run through a speculative execution
+        let result;
+        match f(self) {
+            TMResult::Retry => {
+                return TMResult::Retry;
+            }
+            TMResult::Ok(val) => {
+                if self.is_abort {
+                    return TMResult::Retry;
+                }
+                result = val;
+            }
+        }
+
+        // 3. Lock the write-set
+        if !self.lock_write_set() {
+            return TMResult::Retry;
+        }
+
+        // 4. Increment global version-clock
+        let ver = 1 + self.inc_global_clock();
+
+        // 5. Validate the read-set
+        if !self.validate_read_set() {
+            return TMResult::Retry;
+        }
+
+        // 6. Commit and release the locks
+        self.commit(ver);
+
+        return TMResult::Ok(result);
+    }
+
+    fn lock_write_set(&mut self) -> bool {
+        for (addr, _) in self.write_set.iter() {
+            if self.mem.lock_addr(*addr) {
+                self.locked.push(*addr);
+            } else {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn validate_read_set(&self) -> bool {
+        for addr in self.read_set.iter() {
+            if self.write_set.contains_key(addr) {
+                let ver = self.mem.get_addr_ver(*addr);
+                if ver > self.read_ver {
+                    return false;
+                }
+            } else {
+                if !self.mem.test_not_modify(*addr, self.read_ver) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    fn inc_global_clock(&mut self) -> u64 {
+        self.mem.global_clock.fetch_add(1, Ordering::AcqRel)
+    }
+
+    fn commit(&mut self, ver: u64) {
+        for (addr, val) in self.write_set.iter() {
+            let addr = *addr as usize;
+            for (dst, src) in self.mem.mem[addr..addr + STRIPE_SIZE].iter_mut().zip(val) {
+                *dst = *src;
+            }
+            let idx = addr >> self.mem.shift_size;
+            self.mem.lock_ver[idx].store(ver, Ordering::Release);
+        }
+    }
+}
+
+impl<'a> Drop for WriteTrans<'a> {
+    fn drop(&mut self) {
+        for addr in self.locked.iter() {
+            self.mem.unlock_addr(*addr);
+        }
+    }
+}
+
+pub struct STM {
+    mem: UnsafeCell<Memory>,
+}
+
+unsafe impl Sync for STM {}
+
+impl STM {
+    pub fn new() -> STM {
+        STM {
+            mem: UnsafeCell::new(Memory::new()),
+        }
+    }
+
+    pub fn write_transaction<F, R>(&self, f: F) -> R
+    where
+        F: Fn(&mut WriteTrans) -> TMResult<R>,
+    {
+        loop {
+            let mut tr = WriteTrans::new(unsafe { &mut *self.mem.get() });
+            match tr.transaction(&f) {
+                TMResult::Retry => (),
+                TMResult::Ok(val) => return val,
+            }
+        }
     }
 }
